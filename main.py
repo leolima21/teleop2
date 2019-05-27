5e99c789158a04394c82842ad13a60f4060b761c
Leonardo Lima <50239670+leolima21@users.noreply.github.com>
2019-05-27 17:30:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
+# Bibliotecas necessarias
 from PySide2 import *
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
 import sys
+
+import rospy
+from std_msgs.msg import String
+from std_msgs.msg import Int32
+import time
 
 
 class MainWindow(QMainWindow):
@@ -20,10 +26,10 @@ class MainWindow(QMainWindow):
 		self.but_dir = QPushButton('Turn Right')
 
 		# Atribuicao das funcoes
-		#self.but_frente.clicked.connect(self.move(str('w'))
-		#self.but_tras.clicked.connect(self.move(str('s'))
-		#self.but_esq.clicked.connect(self.move(str('a'))
-		#self.but_dir.clicked.connect(self.move(str('d'))
+		self.but_frente.clicked.connect(publicar('w'))
+		self.but_tras.clicked.connect(publicar('s'))
+		self.but_esq.clicked.connect(publicar('a'))
+		self.but_dir.clicked.connect(publicar('d'))
 
 		# Grid layout 
 		layout = QGridLayout()
@@ -38,30 +44,31 @@ class MainWindow(QMainWindow):
 		widget.setLayout(layout)
 		self.setCentralWidget(widget) 
 
-	def move (self, direction):
-		if (direction == 'w'):
-			print('ERRO')
-		elif (direction == 's'):
-			print('ERRO')
-		elif (direction == 'a'):
-			print('ERRO')
-		elif (direction == 'd'):
-			print('ERRO')
-		else:
-			print('ERRO')
-	
+
+def ros_node():
+	# Objeto que ira publicar no topico teleop_topic
+	pub = rospy.Publisher('teleop_topic', String, queue_size=10)
+
+	# Inicio do node teleop_pub
+	rospy.init_node('teleop_pub', anonymous=True)
+
+
+def publicar(comando):
+	# Publicacao da info no topico
+	rospy.loginfo(comando)
+	pub.publish(comando)
+
+	# Esperar 2s ate o proximo comando
+	time.sleep(2)
+
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
 
 	window = MainWindow()
 
+	ros_node()
+
 	window.show()
 
 	app.exec_()
-
-
-
-
-
-
